be8bcb52c2925ee80b1de4bcda51b531429d26e2
Dweep Bariya <dweepbariya2006@gmail.com>
2026-03-16 02:17:08 +0530

    Rebuild repo with Angular Frontend and JS Backend

--- JS Backend/src/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getAdminUsers, getAdminProgress } = require("../controllers/admin.controller");
-
-const router = express.Router();
-
-router.get("/users", getAdminUsers);
-router.get("/progress", getAdminProgress);
-
-module.exports = router;
--- JS Backend/src/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || "default_jwt_secret_dweep", {
-    expiresIn: "30d",
-  });
-};
-
-module.exports = generateToken;
