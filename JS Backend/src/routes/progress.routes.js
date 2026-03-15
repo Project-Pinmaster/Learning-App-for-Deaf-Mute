674973aa0ccba4937b5e9802161bdd168213a7e4
Dweep Bariya <dweepbariya2006@gmail.com>
2026-03-16 02:12:14 +0530

    Remove old repos

--- JS Backend/src/routes/progress.routes.js
+++
@@ -1,8 +0,0 @@
-﻿const express = require("express");
-const { getUserProgress } = require("../controllers/progress.controller");
-
-const router = express.Router();
-
-router.get("/:userId", getUserProgress);
-
-module.exports = router;
--- JS Backend/src/routes/sign.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const signController = require("../controllers/sign.controller");
-
-router.post("/predict", signController.predictSign);
-
-module.exports = router;
